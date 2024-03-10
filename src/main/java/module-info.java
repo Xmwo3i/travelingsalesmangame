9b5114cf8f2b37448767a4cfab420263abea5053
Xmwo3i <faghanim@uoguelph.ca>
2024-03-10 14:40:39 -0400

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,18 @@
+module com.example.travelingsalesmangame {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+    requires java.desktop;
+    requires java.datatransfer;
+
+    opens com.example.travelingsalesmangame to javafx.fxml;
+    exports com.example.travelingsalesmangame;
+}
